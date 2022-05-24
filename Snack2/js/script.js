bc023075f67044a64d5a0a68d22b01c8505532b3
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-24 15:28:20 +0200

    Snack2 done

---
+++ Snack2/js/script.js
@@ -0,0 +1,34 @@
+// Snack2
+// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi,
+// e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+const executeButton = document.getElementById("execute");
+const nomi = [
+  "armando",
+  "gerardo",
+  "pasquale",
+  "antonia",
+  "gennaro",
+  "filippo",
+  "domenico",
+  "peppino",
+];
+const cognomi = [
+  "impestato",
+  "verdi",
+  "rossi",
+  "marquez",
+  "leclerc",
+  "hamilton",
+  "banana",
+  "fragola",
+];
+
+executeButton.addEventListener("click", function () {
+  document.getElementById("list").innerHTML = "";
+  for (i = 0; i < 10; i++) {
+    document.getElementById("list").innerHTML += `<li>${
+      nomi[Math.floor(Math.random() * nomi.length)]
+    } ${cognomi[Math.floor(Math.random() * cognomi.length)]}</li>`;
+  }
+});
